60cddbf28f7e74ada4ba92f1a7ad97d1fe7b0383
steffenzander <steffen.zander@gmx.net>
2020-02-26 08:35:19 +0100

    Update example-smart-app.js

--- example-smart-app/src/js/example-smart-app.js
+++ example-smart-app/src/js/example-smart-app.js
@@ -17,7 +17,8 @@
                       code: {
                         $or: ['http://loinc.org|8302-2', 'http://loinc.org|8462-4',
                               'http://loinc.org|8480-6', 'http://loinc.org|2085-9',
-                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4']
+                              'http://loinc.org|2089-1', 'http://loinc.org|55284-4',
+							  'http://loinc.org|29463-7', 'http://loinc.org|39156-5']
                       }
                     }
                   });
@@ -41,6 +42,8 @@
           var diastolicbp = getBloodPressureValue(byCodes('55284-4'),'8462-4');
           var hdl = byCodes('2085-9');
           var ldl = byCodes('2089-1');
+		  var weight = byCodes('29463-7');
+		  var bmi = byCodes('39156-5');
 
           var p = defaultPatient();
           p.birthdate = patient.birthDate;
@@ -83,6 +86,8 @@
       diastolicbp: {value: ''},
       ldl: {value: ''},
       hdl: {value: ''},
+	  weight: {value: ''},
+	  bmi: {value: ''},
     };
   }
 
@@ -126,6 +131,8 @@
     $('#diastolicbp').html(p.diastolicbp);
     $('#ldl').html(p.ldl);
     $('#hdl').html(p.hdl);
+	$('#weight').html(p.weight);
+	$('#bmi').html(p.bmi);
   };
 
 })(window);
